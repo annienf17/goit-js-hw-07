1e3a5438a08f189d50629f080b60580fefeaa58b
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-08-10 20:53:47 +0100

    first commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
+"use strict";
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+    // add images to li elements
+const galleryImages = galleryItems.map((img) => 
+`<li class="gallery__item">
+<a class="gallery__link" href="${img.original}">
+<img
+      class="gallery__image"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+    /></a>
+    </li>`).join("");
+
+    // add array of imgs to gallery
+gallery.insertAdjacentHTML("afterbegin", galleryImages);
+
+    // onclick event
+gallery.addEventListener("click", event => {
+    // prevent default browser behaviour
+    event.preventDefault();
+    // if no img finish function
+if(!event.target.dataset.source) {
+    return;
+} 
+    // show image on click
+const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`)
+
+instance.show()
+{
+    // close modal window on click ESCAPE bttn
+    document.addEventListener("keydown", (ev) => {
+    if(ev.key === "Escape") {
+        instance.close();
+    }
+})}
+});
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+    // add images to li elements
+const galleryImages = galleryItems.map((img) => 
+`<li>
+<a class="gallery__item" href="${img.original}">
+<img
+      class="gallery__image"
+      src="${img.preview}"
+      alt="${img.description}"
+    /></a>
+    </li>`).join("");
+
+    // add array of imgs to gallery
+gallery.insertAdjacentHTML("afterbegin", galleryImages);
+
+// SimpleLightbox.com
+let lightbox = new SimpleLightbox('.gallery a', {
+    /* options */
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    showCounter: true,
+    captionDelay: 250,
+  });
+
+
+
